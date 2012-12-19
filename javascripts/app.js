7823ec906183ad6b5eed779f2d82b08f36b60458
Haacked <haacked@gmail.com>
2012-12-19 14:47:01 -0800

    Consolidated annotations into a single file.
    The annotations are now described in app.js. This'll make it much easier
    for us to change these up without having to fix tem up in the entire
    site.

--- javascripts/app.js
+++ javascripts/app.js
@@ -1,9 +1,42 @@
+var annotations = {
+  permitted: {
+    'commercial-use' : 'This software and derivatives may be used for commercial purposes.',
+    'modifications' : 'This software may be modified.',
+    'distribution' : 'You may distribute this software.',
+    'sublicense' : 'You may grant a sublicense to modify and distribute this software to third parties not included in the license.',
+    'warranty' : 'You may place a warranty on the software.'
+  },
+
+  forbidden: {
+    'no-warranty' : 'Software is released without warranty and the software/license owner cannot be charged for damages.',
+    'trademark-use' : 'You may NOT use the names, logos, or trademarks of contributors.',
+    'no-liability' : 'Software is provided without warranty and the software author/license owner cannot be held liable for damages.',
+    'no-sublicense' : 'You may not grant a sublicense to modify and distribute this software to third parties not included in the license.',
+  },
+  
+  required: {
+    'include-copyright' : 'Include the original copyright with the code.',
+    'include-license' : 'Include the full text of the license with the code.',
+    'document-changes' : 'Indicate significant changes made to the code.',
+    'disclose-source' : 'Source code must be made available when distributing the software.',
+    'nonstatic-linkage' : 'The library must be linked in a way that allows it to be replaced with a similar library.'
+  }
+}
+  
+
 var qtip_position = {
   my: 'top center',
   at: 'bottom center'
 };
 
 $(document).ready(function() {
+  for (var category in annotations) {
+    var categoryAnnotations = annotations[category];
+    for (var annotation in categoryAnnotations) {
+      $('table.license ul.' + category + ' li.' + annotation).attr('title', categoryAnnotations[annotation]);
+    }
+  }  
+
   $('table.license ul.required li').qtip({
     content: {
       text: false,
