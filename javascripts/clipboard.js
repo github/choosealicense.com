6220d2ab32b3979c8611ee98be590de3b3303b78
Haacked <haacked@gmail.com>
2013-01-07 12:29:16 -0800

    Implement copy to clipboard
    Closes #19
    Thanks to @jonrohan's ZeroClipboard this was quick and easy!

---
+++ javascripts/clipboard.js
@@ -0,0 +1,20 @@
+$(document).ready(function() {
+
+  // Backup the clipboard button's original text.
+  $("#clipboard-button").data('clipboard-prompt', $('#clipboard-button').text());
+  
+  // Hook up copy to clipboard buttons
+  var clip = new ZeroClipboard( $("#clipboard-button"), {
+    moviePath: "/javascripts/ZeroClipboard.swf"
+  } );
+
+  clip.on( 'mouseover', function(client, args) {
+    // Restore the clipboard button's original text.
+    this.innerText = $(this).data('clipboard-prompt');
+  } );
+
+  clip.on( 'complete', function(client, args) {
+    this.innerText = 'Copied!';
+  } );
+
+});
