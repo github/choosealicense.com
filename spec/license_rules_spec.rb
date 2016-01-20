5531742d10fbd51c07b4488125f32f05815e6b33
Ben Balter <ben.balter@github.com>
2016-01-20 10:20:04 -0500

    better error output

--- spec/license_rules_spec.rb
+++ spec/license_rules_spec.rb
@@ -1,22 +1,24 @@
 require 'spec_helper'
 
-licenses.each do |license|
+describe "license rules" do
+  licenses.each do |license|
 
-  groups = rules.keys
+    groups = rules.keys
 
-  describe "The #{license["title"]} license" do
-    groups.each do |group|
+    context "The #{license["title"]} license" do
+      groups.each do |group|
 
-      valid_tags = rules[group].map { |r| r["tag"] }
+        valid_tags = rules[group].map { |r| r["tag"] }
 
-      describe "the #{group} group" do
-        it "should exist" do
-          expect(license[group]).to_not be_nil
-        end
+        context "the #{group} group" do
+          it "should exist" do
+            expect(license[group]).to_not be_nil
+          end
 
-        it "should only contain valid tags" do
-          extra = license[group] - valid_tags
-          expect(extra).to be_empty
+          it "should only contain valid tags" do
+            extra = license[group] - valid_tags
+            expect(extra).to be_empty
+          end
         end
       end
     end
--- spec/license_spec.rb
+++ spec/license_spec.rb
@@ -1,26 +1,28 @@
 require 'spec_helper'
 
-licenses.each do |license|
+describe "licenses" do
+  licenses.each do |license|
 
-  # "No license" isn't really a license, so no need to test
-  next if license["id"] == "no-license"
+    # "No license" isn't really a license, so no need to test
+    next if license["id"] == "no-license"
 
-  describe "The #{license["title"]} license" do
+    context "The #{license["title"]} license" do
 
-    let(:id) { license["id"]  }
+      let(:id) { license["id"]  }
 
-    it "has an SPDX ID" do
-      expect(spdx_ids).to include(id)
-    end
+      it "has an SPDX ID" do
+        expect(spdx_ids).to include(id)
+      end
 
-    it "uses its SPDX name" do
-      spdx = find_spdx(id)
-      expect(spdx[1]["name"].gsub(/ only$/,"")).to eql(license["title"])
-    end
+      it "uses its SPDX name" do
+        spdx = find_spdx(id)
+        expect(spdx[1]["name"].gsub(/ only$/,"")).to eql(license["title"])
+      end
 
-    context "industry approval" do
-      it "should be approved by OSI or FSF or OD" do
-        expect(approved_licenses).to include(id), "See https://git.io/vzCTV."
+      context "industry approval" do
+        it "should be approved by OSI or FSF or OD" do
+          expect(approved_licenses).to include(id), "See https://git.io/vzCTV."
+        end
       end
     end
   end
