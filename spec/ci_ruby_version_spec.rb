7de7b2c8ef45fa3ca38d0a24318e1ac9f7d53cc9
Mike Linksvayer <mlinksva@github.com>
2017-01-03 10:18:27 -0800

    affirmative wording
    https://github.com/github/choosealicense.com/pull/473#discussion_r94403400

--- spec/ci_ruby_version_spec.rb
+++ spec/ci_ruby_version_spec.rb
@@ -12,8 +12,8 @@ describe 'ci ruby version' do
   ci_config = YAML.load_file(ci_config_file)
   ci_ruby_version = ci_config['rvm'][0]
 
-  context "#{ci_config_file} and pages ruby versions" do
-    it 'do not match' do
+  context "in #{ci_config_file} and pages ruby version" do
+    it 'match' do
       msg = "(#{ci_ruby_version}) != (#{pages_ruby_version})"
       expect(ci_ruby_version).to eql(pages_ruby_version), msg
     end
