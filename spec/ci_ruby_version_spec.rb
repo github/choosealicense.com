fad13d2de4bdd91c1dda5d236a4460018f9a6124
Mike Linksvayer <mlinksva@github.com>
2017-01-04 08:53:18 -0800

    document fixing error in test output
    https://github.com/github/choosealicense.com/pull/473#issuecomment-270185138

--- spec/ci_ruby_version_spec.rb
+++ spec/ci_ruby_version_spec.rb
@@ -14,7 +14,7 @@ describe 'ci ruby version' do
 
   context "in #{ci_config_file} and pages ruby version" do
     it 'match' do
-      msg = "(#{ci_ruby_version}) != (#{pages_ruby_version})"
+      msg = "#{ci_ruby_version} != #{pages_ruby_version}; please add a commit bumping in #{ci_config_file}"
       expect(ci_ruby_version).to eql(pages_ruby_version), msg
     end
   end
