5134e27c79f6b3179b52d66852777bd63f5e1f2e
Mike Linksvayer <mlinksva@github.com>
2017-10-08 20:38:56 -0700

    rubocop

--- spec/license_fields_spec.rb
+++ spec/license_fields_spec.rb
@@ -5,7 +5,7 @@ require 'spec_helper'
 describe 'license fillable fields' do
   licenses.each do |license|
     context "The #{license['title']} license" do
-      matches = license['content'].scan(/(\A|\W)\[([a-z]+)\]/).map {|row| row[1]}
+      matches = license['content'].scan(/(\A|\W)\[([a-z]+)\]/).map { |row| row[1] }
       extra_fields = matches.flatten - (fields.map { |f| f['name'] })
       it 'should only contain supported fillable fields' do
         expect(extra_fields).to be_empty
