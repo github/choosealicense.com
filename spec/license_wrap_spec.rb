dc00fc0ae5b0ed51488397c9dc3f05181771986a
Mike Linksvayer <mlinksva@github.com>
2017-11-19 12:33:01 -0800

    rubocop

--- spec/license_wrap_spec.rb
+++ spec/license_wrap_spec.rb
@@ -9,7 +9,7 @@ describe 'word wrapping' do
         file_content = File.read(file, encoding: 'utf-8')
         license_text = file_content.match(/\A(?:---\n.*\n---\n+)?(.*)/m)[1]
         max_line_length = 0
-        max_line = ""
+        max_line = ''
         license_text.lines.each do |line|
           line.chomp!
           if line.length > max_line_length
