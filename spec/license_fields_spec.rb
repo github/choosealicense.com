316bd0b828f90959475e58b423823bf1e342b27f
Mike Linksvayer <mlinksva@github.com>
2018-01-02 14:35:04 -0800

    match _ fields, require preceding space (ex CECILL-2.1)

--- spec/license_fields_spec.rb
+++ spec/license_fields_spec.rb
@@ -6,7 +6,7 @@ describe 'license fillable fields' do
   licenses.each do |license|
     context "The #{license['title']} license" do
       it 'should only contain supported fillable fields' do
-        matches = license['content'][0, 1000].scan(/\[([a-z]+)\]/)
+        matches = license['content'][1, 1000].scan(/\s+\[([a-z_]+)\]/)
         extra_fields = matches.flatten - (fields.map { |f| f['name'] })
         expect(extra_fields).to be_empty
       end
