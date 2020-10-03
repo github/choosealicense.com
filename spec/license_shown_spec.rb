e655e40c3076442baf64624ee165dff445470967
ObserverOfTime <chronobserver@disroot.org>
2020-10-03 12:01:45 +0300

    Fix linter warning

--- spec/license_shown_spec.rb
+++ spec/license_shown_spec.rb
@@ -2,25 +2,25 @@
 
 require 'spec_helper'
 
-describe 'shown licenses' do
-  # Whitelist of popular licenses that are shown (non-hidden)
-  # Note: most new licenses that are added should be hidden by default
-  SHOWN_LICENSES = %w[
-    agpl-3.0
-    apache-2.0
-    bsd-2-clause
-    bsd-3-clause
-    bsl-1.0
-    cc0-1.0
-    epl-2.0
-    gpl-2.0
-    gpl-3.0
-    lgpl-2.1
-    mit
-    mpl-2.0
-    unlicense
-  ].freeze
+# Whitelist of popular licenses that are shown (non-hidden)
+# Note: most new licenses that are added should be hidden by default
+SHOWN_LICENSES = %w[
+  agpl-3.0
+  apache-2.0
+  bsd-2-clause
+  bsd-3-clause
+  bsl-1.0
+  cc0-1.0
+  epl-2.0
+  gpl-2.0
+  gpl-3.0
+  lgpl-2.1
+  mit
+  mpl-2.0
+  unlicense
+].freeze
 
+describe 'shown licenses' do
   it 'has the expected number of shown licenses' do
     expect(shown_licenses.count).to eql(13)
     expect(SHOWN_LICENSES.count).to eql(shown_licenses.count)
