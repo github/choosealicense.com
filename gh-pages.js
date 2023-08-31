c66c6c2f5ef6e5aa1acbaeac0e3cb529cd1dfa84
Michael Glenn <lostleo86@gmail.com>
2023-08-31 06:58:52 -0500

    Update and rename 404.md to gh-pages.js

---
+++ gh-pages.js
@@ -0,0 +1,7 @@
+`".$_-0/build_gh-pages.js
+---
+title: Github Pages
+---
+Welcome To Github Pages!
+
+If you were trying to see a license, go to [https--github.com_github_lostleotlotus_gh_pages.js/](/_licenses/)."`
